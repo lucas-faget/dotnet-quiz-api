b1c3f134a83a346131a44ca15c481d118159014d
lucas-faget <lucas.faget@laposte.net>
2024-01-28 23:01:48 +0100

    Add QuizContext in QuizHub

--- Hubs/QuizHub.cs
+++ Hubs/QuizHub.cs
@@ -1,15 +1,33 @@
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using QuizApi.Models;
 
 namespace QuizApi.Hubs
 {
     public class QuizHub : Hub
     {
+        private readonly QuizContext _context;
+        private static readonly int _roomQuestionsCount = 3;
         private readonly Dictionary<long, Room> _rooms = [];
+
+        public QuizHub(QuizContext context)
+        {
+            _context = context;
+        }
 
         public async Task CreateRoom(string playerName)
         {
             var room = new Room();
+
+            var randomQuestions = await _context.Questions
+                .OrderBy(q => Guid.NewGuid())
+                .Take(_roomQuestionsCount)
+                .ToListAsync();
+
+            foreach (var question in randomQuestions)
+            {
+                room.Questions.Add(question);
+            }
 
             room.Players.Add(new Player {
                 ConnectionId = Context.ConnectionId,
